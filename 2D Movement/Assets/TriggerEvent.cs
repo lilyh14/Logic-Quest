f60072a0dd63c7ca1a87ff9030f0269e9f1b3a78
lilyh14 <lhinkeldey@ufl.edu>
2019-01-26 02:39:20 -0500

    First commit

---
+++ 2D Movement/Assets/TriggerEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+public string level = "scene2";
+
+void OnTriggerEnter2D(Collider2D collider){
+    if(collider.gameObject.tag == "Player"){
+    Debug.Log("Test");
+    //Application.LoadLevel(level);
+    SceneManager.LoadScene(level);
+        }
+    }
+}
